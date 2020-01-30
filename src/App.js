5af2dfe1184d12320caddc391944830debfe121e
Alicia <aszpunar@infomm.local>
2020-01-30 18:59:14 +0100

    Sesion 34 30.01.2020

--- src/App.js
+++ src/App.js
@@ -2,15 +2,84 @@ import React, {
     Component
 } from 'react'
 import TablaComponentesSimples from './TablaComponentesSimples'
+import TablaComponentesSimplesProps from './TablaComponentesSimplesProps'
+import TablaComponentesSimplesState from './TablaComponentesSimplesState'
 import Tabla from './Tabla';
 class App extends Component {
+    /*Objeto para utilizar en state */
+    state = {
+        personajes:[
+            {
+                nombre:"Payton Hobart"
+            },
+            {
+                nombre:"Wendy Carr"
+            },
+            {
+                nombre:"Mina"
+            },
+            {
+                nombre:"Jonathan Harker"
+            },
+            {
+                nombre:"Drácula"
+            },
+            {
+                nombre:"Once"
+            } ,
+            {
+                nombre:"Jim Hopper"
+            } 
+
+        ]
+    }
+    /*Método para borrar personajes */
+    borrarPersonaje=indice=>{
+        const {personajes} =this.state
+        this.setState(
+            {
+                personajes:personajes.filter((personaje,i)=>{
+                    return i !== indice; /* Devuelve los indices que no queremos borrar */
+                })
+            }
+        )
+    }
     render() {
+        /*Coleccion de objetos con nomenclatura JSON para utilizar con PROPS */
+        const actoresactrices=[
+            {
+                nombre:"Joel",
+                apellidos:"Edger"
+            },
+            {
+                nombre:"Carmen",
+                apellidos:"Maura"
+            },
+            {
+                nombre:"Luis",
+                apellidos:"Tosar"
+            },
+            {
+                nombre:"Chloe",
+                apellidos:"Grace Moretz"
+            }            
+        ]
+
+        /* Inicializamos el objeto de personajes */
+        const {personajes}=this.state
+
         return ( <div className = "App" >
                  <h2> Tabla sin componentes simples </h2> 
                      <Tabla />
                  <h2> Tabla con componentes simples </h2> 
-                    <TablaComponentesSimples /
-             ></div> )
+                    <TablaComponentesSimples />
+                <h2>Tabla creada mediante componente de clase que utiliza paso de parámetros con PROPS</h2>    
+                <TablaComponentesSimplesProps datosActoresActrices={actoresactrices}/>
+
+                <h2>Tabla creada mediante componentes simples (con state y que permite eliminar)</h2>
+                <TablaComponentesSimplesState datosPersonajes={personajes} borrarPersonaje={this.borrarPersonaje} />
+
+                </div> )
     }
 }
 export default App;
---
+++ src/TablaComponentesSimplesState.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+
+const TablaCabecera = () =>{
+    return (
+        <thead> 
+            <tr> 
+                <th>Nombre</th>
+                <th>Borrar</th>
+            </tr> 
+        </thead> 
+    )
+}
+
+const TablaCuerpo = props => {
+    const filasDeDatos=props.datosPersonajes.map((fila, indice)=>{
+        return (
+            <tr key={indice}>
+                 <td>{fila.nombre}</td>
+                <td><button onClick={()=>props.borrarPersonaje(indice)}>Borrar</button></td>
+            </tr>
+        )
+    })
+    return(
+        <tbody>
+            {filasDeDatos}
+        </tbody>
+    )
+}
+
+
+const TablaComponentesSimplesState=(props)=>{
+    const {datosPersonajes,borrarPersonaje}=props;
+    return(
+        <table>
+            <TablaCabecera />
+            <TablaCuerpo datosPersonajes={datosPersonajes} borrarPersonaje={borrarPersonaje}/>
+        </table>
+    )
+}
+export default TablaComponentesSimplesState
+
+
