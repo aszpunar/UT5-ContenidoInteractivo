4c5792d4f25b388ec73b2573a82757859cc3841a
Alicia <aszpunar@infomm.local>
2020-02-03 20:24:43 +0100

    Version 1.0

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ import TablaComponentesSimples from './TablaComponentesSimples'
 import TablaComponentesSimplesProps from './TablaComponentesSimplesProps'
 import TablaComponentesSimplesState from './TablaComponentesSimplesState'
 import Tabla from './Tabla';
+import Formulario from './Formulario';
+// import personajes from "./personajes.json";
 class App extends Component {
     /*Objeto para utilizar en state */
-    state = {
+    /*LO QUE ESTA COMENTADO ES SIN FICHERO JSON*/
+    /*Cuando lo creemos en un fichero JSON aparte  se quita el nombre del array y las comillas son dobles tanto en nombre como en contenido */
+    /*state = {
         personajes:[
             {
                 nombre:"Payton Hobart"
@@ -32,7 +36,20 @@ class App extends Component {
             } 
 
         ]
+    }*/
+    /*Ahora utilizando el fichero externo se haria así: */
+    
+   // state={personajes};
+
+   /* Ahora vamos a hacer un json vacio para añadirle contenido*/ 
+
+   state={personajes : []}
+   
+   //Método para manejar datos del formulario
+   manejarEnvio = personaje => {
+        this.setState({personajes: [...this.state.personajes, personaje]}); 
     }
+
     /*Método para borrar personajes */
     borrarPersonaje=indice=>{
         const {personajes} =this.state
@@ -78,6 +95,9 @@ class App extends Component {
 
                 <h2>Tabla creada mediante componentes simples (con state y que permite eliminar)</h2>
                 <TablaComponentesSimplesState datosPersonajes={personajes} borrarPersonaje={this.borrarPersonaje} />
+
+                <h2>Formulario añadir personajes a JSON</h2>
+                <Formulario manejarEnvio={this.manejarEnvio} />
 
                 </div> )
     }
---
+++ src/Formulario.js
@@ -0,0 +1,63 @@
+import React, {Component} from "react";
+
+class Formulario extends Component {
+    constructor (props){
+        super(props);
+        this.initialState={'name':""};
+        this.state=this.initialState;
+    }
+        manejarCambio=evento =>{
+            const {name, value} = evento.target;
+            this.setState({[name]:value});
+        }
+        enEmvioFormulario=(evento)=>{
+            evento.preventDefault();
+            this.props.manejarEnvio(this.state);
+            this.setState(this.initialState);
+        }
+        render(){
+            const {name}=this.state;
+            return (
+                <form onSubmit={this.enEmvioFormulario}>
+                    <label for="nombreId">Nombre</label>
+                    <input type="text" name="name" id="nombreId" value={name} onChange={this.manejarCambio}/>
+                    <button type="submit">Enviar Datos</button>
+                </form>
+            );
+        }
+    }
+
+
+/*class Formulario extends Component {
+    constructor(props) {
+        super(props);
+        this.initialState = {
+            name: ''
+        };
+        this.state = this.initialState;
+    }
+    manejarCambio = evento => {
+        const {
+            name,
+            value
+        } = evento.target;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    enEnvioFormulario = (evento) => {
+        evento.preventDefault();
+        this.props.manejarEnvio(this.state);
+        this.setState(this.initialState);
+    }
+    render() {
+        const {
+            name
+        } = this.state;
+        return ( <form onSubmit = {this.enEnvioFormulario}> 
+        <label for="nombreid"> Nombre </label> 
+        <input type="text" name="name" id="nombreid" value={name} onChange={this.manejarCambio} /> <button type = "submit" > Enviar Datos </button> </form > );
+    }
+}*/
+export default Formulario
